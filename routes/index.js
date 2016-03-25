f42ae2b0ab5590f800db64204e60e54d7c68f333
David Landreman <david.landreman@crosschx.com>
2016-03-24 22:36:35 -0400

    - Converted to a nodejs / express web project

---
+++ routes/index.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var phridge = require('phridge');
+var router = express.Router();
+
+var phantom;
+var page;
+
+phridge.spawn({
+}).then(function (data) {
+  console.log("PhantomJS Started");
+  phantom = data;
+  phantom.openPage("https://www.google.com/").then(function(data) {
+    try {
+      page = data;
+      console.log("Website Loaded");
+      page.run(function() {
+        try {
+          console.log("Sizing Page");
+          this.viewportSize = {width: 1024, height: 728};
+          console.log("Adding JQuery to page");
+          this.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js");
+          console.log("Rendering page");
+          this.render("tmp/output.png");
+        } catch(ex) {
+          console.log(ex);
+        }
+      }).then(function() {
+        console.log("Page Rendered");
+      })
+    } catch (ex) {
+      console.log(ex);
+    }
+  });
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/data', function(req, res, next) {
+  res.send({test:true});
+});
+
+router.get("/layout", function(req,res, next) {
+  page.run(function() {
+    return this.evaluate(function() {
+      var elements = [];
+      jQuery("*").each(function(index, element) {
+        try {
+          if((jQuery(element).width() > 20 || jQuery(element).height() > 20)) {
+            elements.push({position: jQuery(element).position(),
+              width:jQuery(element).width(),
+              height: jQuery(element).height(),
+              //text: jQuery('*:not(:has(*)):visible', element).text(),
+              type: jQuery(element).get(0).tagName});
+          }
+        } catch (ex) {}
+      });
+      return elements;
+    })
+  }).then(function(result) {
+    res.send({data: result});
+  })
+});
+
+module.exports = router;
